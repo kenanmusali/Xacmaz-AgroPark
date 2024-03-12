6c3b21e282d5e1b44316c0156c33935fdfcfd79d
kenanmusali <textme0306@gmail.com>
2024-03-12 05:25:53 -0700

    0.1 (File has been created)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Layout'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      App
+    </div>
+  )
+}
+
+export default App
