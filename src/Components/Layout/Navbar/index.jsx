dc4038cb9681eb077aa61738f1b485cdd389c66c
kenanmusali <textme0306@gmail.com>
2024-03-15 23:37:45 +0400

    0.1 (Uptaded)

--- src/Components/Layout/Navbar/index.jsx
+++ src/Components/Layout/Navbar/index.jsx
@@ -4,7 +4,22 @@ import { IoMailOutline } from "react-icons/io5";
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { HiMenuAlt2 } from "react-icons/hi";
+// import { Link } from 'react-router-dom';
 
+// Navbar Hamburger Menu
+function toggleMenu() {
+  var topnav = document.querySelector('.topnav');
+  var myLinks = document.getElementById('myLinks');
+  topnav.classList.toggle('show');
+  if (myLinks.style.height === '0px') {
+    myLinks.style.height = '100vh';
+  } else {
+    myLinks.style.height = '0';
+  }
+}
+
+
+// Navbar
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
 
@@ -26,7 +41,7 @@ function Navbar() {
 
   return (
     <div className={`Navbar ${isScrolled ? 'scrolled' : ''}`}>
-      <div className={`TopSection ${isScrolled ? 'none':''}`}>
+      <div className={`TopSection ${isScrolled ? 'none' : ''}`}>
         <p className='text'>+994 50 123 45 67 / For More Info, Contact Us</p>
         <div className="navSocialSection">
           <IoMailOutline className='icon1' />
@@ -36,16 +51,37 @@ function Navbar() {
       </div>
 
       <div className="BottomSection">
-        <img className='logo' src="./Public/Favicon/Logo.svg" alt="Xacmaz Aqro Park Logo" />
+        <div className="logoSec">
+          <img className='logo' src="./Public/Favicon/Logo.svg" alt="Xacmaz Aqro Park Logo" />
+          <p className='logotext'>Xaçmaz AqroPark</p>
+        </div>
         <div className="MenuSection">
           <div className="LanguageSection">
-            <a href="">En</a>
-            <p>/</p>
-            <a href="">Ru</a>
-            <p>/</p>
-            <a href="">Az</a>
+            <a href='#'>Məhsullar</a>
+            <a href='#'>Xəbərlər</a>
+            <a href='#'>Qalereya</a>
+            <a href='#'>Vakansiyalar</a>
+            <a href='#'>Əlaqə</a>
           </div>
-          <HiMenuAlt2 className='icon4' />
+
+
+          {/* <HiMenuAlt2 className='icon4' onClick={toggleMenu} /> */}
+
+          {/* <div className="topnav">
+            <div className="mSection">
+              <div id="myLinks" className='Links'>
+                <a className='top' href='#'>Haqqımıda</a>
+               
+              </div>
+              <div className='pDiv' id='myLinks'>
+                <p className='menutext'>Xaçmaz AqroPark</p>
+                <p className='menutext1 bottom'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+                <p className='menutext1'>+994 50 123 45 67 / +994 70 123 45 67</p>
+                <p className='menutext1'>Lorem Ipsum is simply dummy text of the printing</p>
+              </div>
+            </div>
+          </div> */}
+
         </div>
       </div>
     </div>
