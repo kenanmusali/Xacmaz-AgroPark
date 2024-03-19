1c73a4d3f9ae093fdfa1560e1533dfa168fbf613
kenanmusali <textme0306@gmail.com>
2024-03-19 22:38:28 +0400

    0.1 (NotFound Page Uptaded)

--- src/Components/NotFound/index.jsx
+++ src/Components/NotFound/index.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import './style.css'
 import './responsive.css'
 import Notfound404 from '../../../Public/Images/404.png'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
     <div className='NotfoundPage'>
-        <img className='Error404' src={Notfound404} alt="404"/>
+      <div className="divNotfoudgrouptext">
+        <p className='Notfoundtext'>Xaçmaz Aqro Biznes</p>
+        <p className='Notfoundtext1'>Nə isə düzgün getmədi.... Səhifəni yeniləyin..!</p>
+        <Link to="/" className='stylistic'>Ana səhifə</Link>
+      </div>
+      <img className='Error404' src={Notfound404} alt="404" />
     </div>
   )
 }
