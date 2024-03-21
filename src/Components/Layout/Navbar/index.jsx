22ae6a0aeb0365817a3c05c9850217340fb22323
kenanmusali <textme0306@gmail.com>
2024-03-21 16:22:24 +0400

    0.1 (Responsive Uptaded)

--- src/Components/Layout/Navbar/index.jsx
+++ src/Components/Layout/Navbar/index.jsx
@@ -1,23 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
+import './responsive.css';
 import EmailIcon from '@mui/icons-material/Email';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import { Link } from 'react-router-dom';
-// import { HiMenuAlt2 } from "react-icons/hi";
-// import { Link } from 'react-router-dom';
 
 // Navbar Hamburger Menu
-// function toggleMenu() {
-//   var topnav = document.querySelector('.topnav');
-//   var myLinks = document.getElementById('myLinks');
-//   topnav.classList.toggle('show');
-//   if (myLinks.style.height === '0px') {
-//     myLinks.style.height = '100vh';
-//   } else {
-//     myLinks.style.height = '0';
-//   }
-// }
+function toggleMenu() {
+  var body = document.body;
+  var myLinks = document.getElementById('myLinks');
+  var scrollDisabled = body.classList.contains('scroll-disabled');
+  var hamMenuIcon = document.querySelector('.HamMenuIcon');
+  var TopSection = document.querySelector('.TopSection'); 
+  
+  if (!scrollDisabled) {
+    body.classList.add('scroll-disabled');
+    body.style.overflow = 'hidden';
+    myLinks.style.height = '100vh';
+    hamMenuIcon.style.color = 'white';
+    TopSection.style.display = 'none'; 
+  } else {
+    body.classList.remove('scroll-disabled');
+    body.style.overflow = '';
+    myLinks.style.height = '0';
+    hamMenuIcon.style.color = '';
+    TopSection.style.display = ''; 
+  }
+}
+
+
+
 
 
 // Navbar
@@ -45,9 +59,9 @@ function Navbar() {
       <div className={`TopSection ${isScrolled ? 'none' : ''}`}>
         <p className='text'>+994 50 123 45 67 / For More Info, Contact Us</p>
         <div className="navSocialSection">
-          <EmailIcon className='icon1'/>
-          <InstagramIcon className='icon1'/>
-          <FacebookOutlinedIcon className='icon1'/>
+          <EmailIcon className='icon1' />
+          <InstagramIcon className='icon1' />
+          <FacebookOutlinedIcon className='icon1' />
         </div>
       </div>
 
@@ -65,27 +79,35 @@ function Navbar() {
             <Link to='/careers'>Vakansiyalar</Link>
             <Link to='/contact'>Əlaqə</Link>
           </div>
+          <MenuRoundedIcon className='HamMenuIcon' onClick={toggleMenu} />
 
 
-          {/* <HiMenuAlt2 className='icon4' onClick={toggleMenu} /> */}
 
-          {/* <div className="topnav">
+          <div className="topnav">
             <div className="mSection">
               <div id="myLinks" className='Links'>
-                <a className='top' href='#'>Haqqımıda</a>
-               
-              </div>
-              <div className='pDiv' id='myLinks'>
+                <Link className='Nunito topChildMargin' to='/about'>Haqqımıda</Link>
+                <Link className='Nunito' to='/products'>Məhsullar</Link>
+                <Link className='Nunito' to='/news'>Xəbərlər</Link>
+                <Link className='Nunito' to='/gallery'>Qalereya</Link>
+                <Link className='Nunito' to='/careers'>Vakansiyalar</Link>
+                <Link className='Nunito' to='/contact'>Əlaqə</Link>
                 <p className='menutext'>Xaçmaz AqroPark</p>
                 <p className='menutext1 bottom'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
                 <p className='menutext1'>+994 50 123 45 67 / +994 70 123 45 67</p>
                 <p className='menutext1'>Lorem Ipsum is simply dummy text of the printing</p>
+                <div className="navSocialSection bottom">
+                  <EmailIcon className='icon1' />
+                  <InstagramIcon className='icon1' />
+                  <FacebookOutlinedIcon className='icon1' />
+                </div>
               </div>
             </div>
-          </div> */}
+          </div>
 
         </div>
       </div>
+
     </div>
   );
 }
