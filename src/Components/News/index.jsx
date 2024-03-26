77f3efd1567ae5929809daa5a4e0d02564f64086
kenanmusali <textme0306@gmail.com>
2024-03-26 12:18:17 +0400

    0.2 (News Section Uptaded)

--- src/Components/News/index.jsx
+++ src/Components/News/index.jsx
@@ -1,29 +1,71 @@
-import React from 'react'
-import './style.css'
-import './responsive.css'
-import { Link } from 'react-router-dom'
-import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
+import React from "react";
+import "./style.css";
+import "./responsive.css";
+import { Link } from "react-router-dom";
+import KeyboardArrowRightRoundedIcon from "@mui/icons-material/KeyboardArrowRightRounded";
+import WatchLaterRoundedIcon from "@mui/icons-material/WatchLaterRounded";
+import NewsImage1 from "../../../Public/Images/Rectangle22.png";
+import { useState, useEffect } from "react";
+// const url = "http://localhost:3001/News";
 
-function NewsSection() {
+const NewsSection = () => {
+  const [text, setText] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/News")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setText(data);
+      });
+  }, []);
+
+  // function NewsSection() {
   return (
     <div>
       <div className="DivMenuSectionHeaderNews">
         <div className="DivTopNavShort">
-          <Link to='/' className='LinkNav'>Ana Səhifə</Link>
-          <KeyboardArrowRightRoundedIcon className='IconRight' />
-          <p className='RecentMenu'>Xəbərlər</p>
+          <Link to="/" className="LinkNav">
+            Ana Səhifə
+          </Link>
+          <KeyboardArrowRightRoundedIcon className="IconRight" />
+          <p className="RecentMenu">Xəbərlər</p>
         </div>
         <div className="DivBottomtextShort">
           <p className="bckText Menutexts">Xəbərlər</p>
-          <p className="bckText1 Menutexts">Biz aqrobiznes kommunikasiyaları ilə məşğul oluruq</p>
+          <p className="bckText1 Menutexts">
+            Biz aqrobiznes kommunikasiyaları ilə məşğul oluruq
+          </p>
         </div>
       </div>
       {/* You Will start coding here*/}
 
+      
+      {text.map(({ id, text, desc, time, img, klas }) => (
+     
+        <div className="divNewsAddBarContainer">
+          <div className="divbarnewssection1">
+            <div key={id} className={klas}>
+              <img className="NewsImage1" src={img} />
+              <div className="divNewsTextsSections">
+                <p className="NewsText1">{text}</p>
 
+                <p className="Newsdecp1">{desc}</p>
+                <div className="divNewsTimeZone">
+                  <WatchLaterRoundedIcon className="NewsIconSet1" />
+                  <p className="divNewsTime">{time}</p>
+                </div>
+              </div>
+            </div>
+           
+          </div>
+        </div>
+        
+      
+))}
+      </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default NewsSection
+export default NewsSection;
