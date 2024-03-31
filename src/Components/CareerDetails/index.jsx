f5251bb47ef259609c607de629e183eeb6077c28
kenanmusali <textme0306@gmail.com>
2024-04-01 01:54:40 +0400

    0.3 (Career Uptaded)

--- src/Components/CareerDetails/index.jsx
+++ src/Components/CareerDetails/index.jsx
@@ -3,16 +3,25 @@ import './style.css';
 import './responsive.css';
 import { Link, useParams } from 'react-router-dom';
 import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
 
 function CareerDetailSection() {
   const { id } = useParams();
   const [career, setCareer] = useState({});
-  
+  const [isCareerFormVisible, setCareerFormVisible] = useState(false);
+
   useEffect(() => {
     fetch(`http://localhost:3001/Careers/${id}`)
       .then(res => res.json())
       .then(data => setCareer(data));
   }, [id]);
+
+  const handleCareerFormToggle = () => {
+    setCareerFormVisible(!isCareerFormVisible);
+  };
 
   return (
     <div>
@@ -51,9 +60,54 @@ function CareerDetailSection() {
             <p className='divcareertexts1'><span>• </span>{career.article3?.text3}</p>
             <p className='divcareertexts1'><span>• </span>{career.article3?.text4}</p>
           </div>
-          <button type="button" className='SubmitCvBtn'>Müraciət et</button>
+          <button type="button" className='SubmitCvBtn' onClick={handleCareerFormToggle}>Müraciət et</button>
+        </div>
+        {isCareerFormVisible && (
+        <div className="divSubmitCareerSectionbtnGroup">
+          <div className="divSubmitCareerSectionbtn">
+            <div className="divicontextgroupcareer">
+              <p className='divcareertexts'>Məlumatlar</p>
+              <CloseRoundedIcon className='closeIconCareer' onClick={handleCareerFormToggle}/>
+            </div>
+            <input type="text" className='CareerSubmit' placeholder='Ad*' />
+            <input type="text" className='CareerSubmit' placeholder='Soyad*' />
+            <input type="text" className='CareerSubmit' placeholder='Doğum Tarixi*' />
+            <input type="text" className='CareerSubmit' placeholder='İş Təcrübəsi*' />
+            <input type="text" className='CareerSubmit' placeholder='Email*' />
+            <input type="text" className='CareerSubmit' placeholder='Telefon*' />
+            <button type="button" className='SubmitCvBtn' onClick={handleCareerFormToggle}>Göndər</button>
+          </div>
+        </div>
+      )}
+      </div>
+
+      
+      <div className="divNewsAddBarContainer reset00">
+        <div className="divbarnewssection2 reset0">
+          <div className="divCareerBarContainer ">
+            <div className="divcareerbar1">
+              <p className="NewsText1 Nunito">Əlaqə</p>
+              <p className="Nunito Newsdecp1">Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+              <div className="divNewsTimeZone reset reset1">
+                <p className="divNewsTime">+994 50 123 45 67</p>
+                <p className="divNewsTime">+994 70 123 45 67</p>
+                <p className="divNewsTime">info@xacmazagro.az</p>
+              </div>
+            </div>
+            <div className="divLine"></div>
+            <div className="divcareerbar1">
+              <p className="NewsText1 Nunito">Bizi izlə!</p>
+              <div className="divNewsTimeZone reset">
+                <EmailIcon />
+                <InstagramIcon />
+                <FacebookOutlinedIcon />
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
+
     </div>
   );
 }
