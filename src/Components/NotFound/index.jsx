8b43d637d6834138b71213db1ca0920ef5206de6
kenanmusali <textme0306@gmail.com>
2024-05-09 13:52:38 +0400

    0.6.9.5 (404 Page update has been completed.)

--- src/Components/NotFound/index.jsx
+++ src/Components/NotFound/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
 import Notfound404 from '../../../Public/Images/404.png'
+import { LanguageContext } from '../../Assets/Language';
 import { Link } from 'react-router-dom'
 
 function NotFound() {
+
+  const { language } = useContext(LanguageContext);
+
   return (
     <div className='NotfoundPage'>
       <div className="divNotfoudgrouptext">
-        <p className='Notfoundtext'>Xaçmaz Aqro Biznes</p>
-        <p className='Notfoundtext1'>Nə isə düzgün getmədi.... Səhifəni yeniləyin..!</p>
-        <Link to="/" className='stylistic'>Ana səhifə</Link>
+        <p className='Notfoundtext'>{language === 'az' ? 'Xaçmaz Aqro Biznes' : 'Xaçmaz Aqro Business'}</p>
+        <p className='Notfoundtext1'>{language === 'az' ? 'Nə isə düzgün getmədi.... Səhifəni yeniləyin..!' : 'Something went wrong.... Refresh the page..!'}</p>
+        <Link to="/" className='stylistic'>{language === 'az' ? 'Ana səhifə' : 'Home page'}</Link>
       </div>
       <img className='Error404' src={Notfound404} alt="404" />
     </div>
