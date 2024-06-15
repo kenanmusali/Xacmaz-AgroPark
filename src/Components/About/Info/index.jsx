f5e6032cbac30a7133d7ac73887b3b34964bb290
kenanmusali <textme0306@gmail.com>
2024-06-15 19:28:51 +0400

    0.8.5 (About Us page Updated)

--- src/Components/About/Info/index.jsx
+++ src/Components/About/Info/index.jsx
@@ -2,8 +2,6 @@ import React, { useContext } from 'react';
 import { LanguageContext } from '../../../Assets/Language';
 import './style.css';
 import HomeImage1 from '../../../../Public/Images/image191.png'
-import { Link } from 'react-router-dom';
-
 
 function Info() {
   const { language } = useContext(LanguageContext);
@@ -15,7 +13,6 @@ function Info() {
           <p className='Nunito HomeSectionText1 HomeSectionText01'>{language === 'az' ? 'Haqqımızda' : 'About Us'}</p>
           <p className='HomeSectionText2 HomeSectionText02'>{language === 'az' ? 'Xaçmaz Aqro Biznes' : 'Xaçmaz Aqro Business'}</p>
           <p className='Nunito HomeSectionText3'>{language === 'az' ? '2021-ci ildən bəri ailəmizə məxsus fermer şirkətimiz davamlı kənd təsərrüfatında qabaqcıl olmuşdur və yüksək keyfiyyətli məhsulların etibarlı təchizatçısına çevrilmişdir. İnnovasiyalara və ətraf mühitin mühafizəsinə sadiq olaraq, günümüzün çağırışlarına cavab verərkən köklərimizə hörmət etməyə davam edirik.' : 'Since 1987, our family-owned farming company has been a pioneer in sustainable agriculture, growing into a trusted provider of premium produce. Committed to innovation and environmental stewardship, we continue to honor our roots while meeting the challenges of today.'}</p>
-          <Link to={'/about'} className='Nunito bckBtn' >{language === 'az' ? 'Ətraflı' : 'See More'}</Link>
         </div>
         <div className="GroupAboutSectionHomeMenuImg">
           <div className="divAboutSectionHomeMenuImg">
